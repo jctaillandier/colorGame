0ede4003ff822cdd11d8f1d118c971a2e3b4166f
J-C <jc.taillandier@hotmail.com>
2018-07-03 13:47:42 +0800

    Can't figure out bugs (list top of .js file)

--- README.txt
+++ README.txt
@@ -1,8 +1,14 @@
 ---- First Project using JS DOM manipulation -----
 
-- Simple Game to practice RGB color codes
+- Simple Game to practice RGB color codes with a few adjustments
+
 
 i) I give you the RGB code, and 6 options.
 ii)Clicking on a wrong option will make that option dissapear, leaving you with one less
 iii) Clicking on the right option will notify you of success
 iv) Restart button on left
+
+Select difficulty by clicking on appropriate word
+   - Easy mode makes a complete random selection of 6 colors 
+  
+   - Hard mode reduces the range of each R, G and B to +/- 75 from the correct color code
--- color.js
+++ color.js
@@ -1,31 +1,82 @@
+/**
+ *      BUGSS
+ *  1) When user found color, continuous clicking on other boxes increases score because color matches
+ *              Partially fixed. Can't score, but blocks dissapear again, no further issues
+ *  2) hard mode -> cannot 'win', system doesnt go in if statement where color match
+ *              Can't figure out under what circumstances bug appear
+ */
+
+
 var squares = document.querySelectorAll(".square")
 var theTitle = document.querySelector("span");
 var isCorrect = document.querySelector("#correct");
 var topDiv = document.querySelector("#infoBeginning");
+var topdivColor = topDiv.style.backgroundColor;
 var easy = document.querySelector("#easy");
 var hard = document.querySelector("#hard");
+var playerScoreSpan = document.querySelector("#pScore");
+var playerScore = playerScoreSpan.textContent;
+var tries = 0;
 var pickedColor;
 var easyGame = true;
 sixColors = [];
 
-setnewColorsEasy();
+initListeners();
+init();
 
-easy.addEventListener("click" , function(){
-    easy.classList.add("easyHard");
-    hard.classList.remove("easyHard");
-    easyGame = true;
 
-    //start new game with new settings
-    setnewColorsEasy();    
-});
-hard.addEventListener("click" , function(){
-    easy.classList.remove("easyHard");
-    hard.classList.add("easyHard");
-    easyGame = false;
+function initListeners(){
+    //event listeners on 'Easy' Button for both hover and click
+    // easy.addEventListener("mouseover" , function(){
+    //     easy.classList.toggle("hovering")
+    // });
+    easy.addEventListener("click" , function(){
+        easy.classList.add("easyHard");
+        hard.classList.remove("easyHard");
+        easyGame = true;
 
-    //start new game with new settings
-    setnewColorsEasy();
-});
+        //score back to 0
+        playerScoreSpan.textContent = 0;
+        playerScore = 0;
+
+        //start new game with new settings
+        init();    
+    });
+    //same for 'Hard' Button
+    // hard.addEventListener("mouseover" , function(){
+    //     hard.classList.toggle("hovering")
+    // });
+    hard.addEventListener("click" , function(){
+        easy.classList.remove("easyHard");
+        hard.classList.add("easyHard");
+        easyGame = false;
+
+        //score back to 0
+        playerScoreSpan.textContent = 0;
+        playerScore = 0;
+
+        //start new game with new settings
+        init();
+    });
+
+    //event listeners for squares
+    for(var i = 0 ; i < squares.length ; i ++){
+        squares[i].addEventListener("click" , function(){
+            var rgb = this.style.backgroundColor;
+            if( rgb == pickedColor){
+                colorFound();
+            }
+            else{
+                this.style.backgroundColor = "rgb(0, 0, 0, 0.0)";
+                isCorrect.innerHTML = "<strong><em> Try Again. </em></strong>";
+                tries ++;
+            }
+        });
+    }
+}
+
+
+
 
 function randColorEasy(){
     var color = (Math.random() * 255);
@@ -33,12 +84,15 @@ function randColorEasy(){
     return color;
 }
 
-function setnewColorsEasy(){
+//Setting new colors to each squares
+// function called whenever 'easy' 'hard' or 'new game' button are pressed 
+function init(){
+
+    //topDiv.style.backgroundColor = "#F8EAB";
+    isCorrect.innerHTML = "";
+    tries = 0;
 
     if(easyGame == true){
-        topDiv.style.backgroundColor = parseInt(randColorEasy());
-        isCorrect.innerHTML = "";
-
 
         for (var i = 0 ; i < squares.length ; i++){
             var r = parseInt(randColorEasy());
@@ -59,17 +113,22 @@ function setnewColorsEasy(){
     else{
         setnewColorsHard();
     }
+    topDiv.style.backgroundColor = pickedColor;
 }
 
 function setnewColorsHard(){
-
+    tries = 0;
     var r = parseInt(randColorEasy());
     var minR;
+    var maxR;
     var g = parseInt(randColorEasy());
     var minG;
+    var maxG;
     var b = parseInt(randColorEasy());
     var minB;
+    var MaxB;
     sixColors[0] = "rgb(" + r + ", " + g + ", "+ b + ")";
+    var answer = "rgb(" + r + ", " + g + ", "+ b + ")";
 
     //ensure no negative color code appear
     if(r < 75){minR = 0}
@@ -78,12 +137,19 @@ function setnewColorsHard(){
     else{minG = g-75}
     if(b < 75){minB = 0}
     else{minB = b-75}
+    //ensures no above 255
+    if(r >179){maxR = 255}
+    else{maxR = r+75}
+    if(g >180){maxG = 255}
+    else{maxG = g+75}
+    if(b >180){maxb = 255}
+    else{maxb = b+75}
 
     for (var i = 0 ; i < 5 ; i ++){
         
-        rx = parseInt( Math.random() * ((r+75) - minR) + minR );
-        gx = parseInt( Math.random() * ((g+75) - (minG)) + (minG) );
-        bx = parseInt( Math.random() * ((b+75) - (minB)) + (minB) );
+        rx = parseInt( Math.random() * ((maxR) - minR) + minR );
+        gx = parseInt( Math.random() * ((maxR) - (minG)) + (minG) );
+        bx = parseInt( Math.random() * ((maxR) - (minB)) + (minB) );
 
         var rgb ="rgb(" + rx + ", " + gx + ", "+ bx + ")"; 
         sixColors[i+1] = rgb;
@@ -103,18 +169,16 @@ function colorFound(){
     }
     isCorrect.innerHTML = "<strong><em>Correct !!! </em></strong>";
     topDiv.style.backgroundColor = pickedColor;
+
+//line to avoid multiple point clicking on all right colors
+    pickedColor = 0;
+
+    //If the choice was this round's first, then score increases by one
+    if(tries == 0){
+        playerScoreSpan.textContent = ++playerScore;
+    }
 }
 
 
-//event listeners for squares
-for(var i = 0 ; i < squares.length ; i ++){
-    squares[i].addEventListener("click" , function(){
-        var rgb = this.style.backgroundColor;
-        if( rgb == pickedColor){
-            colorFound();
-        }
-        else{
-            this.style.backgroundColor = "rgb(0, 0, 0, 0.0)";
-        }
-    });
-}
+
+
